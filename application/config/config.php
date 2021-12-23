6329d1a5d1cfb0ab163f2c95cdc2aaaadda09afc
riyanris <riyanrisky129@gmail.com>
2021-12-23 21:14:25 +0700

    fix ogin

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/SPK-AHP/';
+// $config['base_url'] = 'http://localhost/SPK-AHP/';
+$config['base_url'] = 'https://stormy-tundra-82570.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
